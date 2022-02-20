a7bd5374472c590f6469e410019dc0be204a7034
Mauro Giuffo <maurogiuffo@gmail.com>
2022-02-20 04:32:48 -0300

    user create

--- server.php
+++ server.php
@@ -8,6 +8,26 @@ if($request->route == "api/testword")
 	response(200,"todo ok", $result);
 }
 
+if($request->route == "api/login")
+{
+	$result = login($request->userid);
+	response(200,"todo ok", $result);
+}
+
+if($request->route == "api/adduser")
+{
+	$result = addUser($request->userid,$request->name,$request->mail);
+	response(200,"todo ok", $result);
+}
+
+function login($userid)
+{
+	$result['state'] = true;
+	$result['userid'] = $userid;
+	//$result['message'] = $message;
+	return $result;
+}
+
 function testWord($testWord)
 {
 	$testWord = strtoupper($testWord);
@@ -57,6 +77,20 @@ function getRealWord()
 }
 
 
+function addUser($userid,$name,$mail)
+{
+	$query = "INSERT INTO `users` (`id`, `name`, `mail`) VALUES ('%d', '%s', '%s')";
+	$query = sprintf($query, $userid,$name,$mail);
+	$res = dbquery($query);
+
+	$result['state'] = $res;
+	//$result['message'] = $res;
+	return $result;
+
+}
+
+
+
 function response($status,$status_message,$data)
 {
 	header("HTTP/1.1 ".$status);
@@ -68,3 +102,31 @@ function response($status,$status_message,$data)
 	$json_response = json_encode($response);
 	echo $json_response;
 }
+
+function dbquery($sql){
+	$host="localhost";        # host name or ip address
+	$user="root";            # database user name
+	$pass="";    # database password
+	$database="wordle";        # dateabase name with which you want to connect
+	$conn = new mysqli($host, $user, $pass, $database);
+	if ($conn->connect_error) {
+	  die("Connection failed: " . $conn->connect_error);
+	}
+	$result = $conn->query($sql);
+	$conn->close();
+	return $result;
+}
+
+/*
+//"SELECT id, firstname, lastname FROM MyGuests";
+if ($result->num_rows > 0) {
+	  // output data of each row
+	  while($row = $result->fetch_assoc()) {
+	    echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+	  }
+	} else {
+	  echo "0 results";
+	}
+
+*/
+
